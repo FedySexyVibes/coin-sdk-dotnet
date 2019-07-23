dab5efaa0bfc959a4659ccf148a3add5826f0806
pauls <pauls@coin.nl>
2019-07-23 10:48:44 +0200

    bumped to version 0.0.2

--- common-sdk/src/common/SdkInfo.cs
+++ common-sdk/src/common/SdkInfo.cs
@@ -2,6 +2,6 @@ namespace Coin.Sdk.Common
 {
     public class SdkInfo
     {
-        public static string UserAgent = "coin-sdk-dotnet-0.0.1";
+        public static string UserAgent = "coin-sdk-dotnet-0.0.2";
     }
 }
