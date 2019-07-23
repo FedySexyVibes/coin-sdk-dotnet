2737ea0e911002a7b4eff42daeb20f69560d2446
pauls <pauls@coin.nl>
2019-07-23 10:51:48 +0200

    CPC-443 Added comment that the version is maitained by the makefile.

--- common-sdk/src/common/SdkInfo.cs
+++ common-sdk/src/common/SdkInfo.cs
@@ -2,6 +2,7 @@ namespace Coin.Sdk.Common
 {
     public class SdkInfo
     {
+        // NOTE: this version is maintained by the makefile, do not modify or move to another file
         public static string UserAgent = "coin-sdk-dotnet-0.0.2";
     }
 }
