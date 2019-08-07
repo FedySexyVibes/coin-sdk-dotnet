75aedc1fe698ce95f84f99b3f6ff7dde7bf81f11
pauls <pauls@coin.nl>
2019-08-07 07:57:12 +0200

    bumped to version 0.0.3

--- common-sdk/src/common/SdkInfo.cs
+++ common-sdk/src/common/SdkInfo.cs
@@ -3,6 +3,6 @@ namespace Coin.Sdk.Common
     public class SdkInfo
     {
         // NOTE: this version is maintained by the makefile, do not modify or move to another file
-        public static string UserAgent = "coin-sdk-dotnet-0.0.2";
+        public static string UserAgent = "coin-sdk-dotnet-0.0.3";
     }
 }
