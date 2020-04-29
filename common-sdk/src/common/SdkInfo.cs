992f90fa86a071f3c599d7af1739cea7889b81d8
RobIII <r.janssen@keensystems.eu>
2020-04-29 10:16:35 +0200

    CA1052: Static holder types should be Static or NotInheritable

--- common-sdk/src/common/SdkInfo.cs
+++ common-sdk/src/common/SdkInfo.cs
@@ -1,6 +1,6 @@
 namespace Coin.Sdk.Common
 {
-    public class SdkInfo
+    public static class SdkInfo
     {
         // NOTE: this version is maintained by the makefile, do not modify or move to another file
         public const string UserAgent = "coin-sdk-dotnet-0.0.6";
