e0ed6a81a6c598669aaf1bf123af9d4d39ec55c7
bart.vanmunster <b.vanmunster@coin.nl>
2020-08-31 17:56:04 +0200

    bumped to version 1.0.7

--- common-sdk/src/common/SdkInfo.cs
+++ common-sdk/src/common/SdkInfo.cs
@@ -3,6 +3,6 @@ namespace Coin.Sdk.Common
     public static class SdkInfo
     {
         // NOTE: this version is maintained by the makefile, do not modify or move to another file
-        public const string UserAgent = "coin-sdk-dotnet-1.0.6";
+        public const string UserAgent = "coin-sdk-dotnet-1.0.7";
     }
 }
