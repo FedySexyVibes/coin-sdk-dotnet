ffbe0a033612d49c91f1d16d07749782e498aa40
Gilles Wijnker <g.wijnker@coin.nl>
2020-11-17 09:52:10 +0100

    CPC-919 Added test for stream interrupt

--- number-portability-sdk-tests/MessageConsumerTests.cs
+++ number-portability-sdk-tests/MessageConsumerTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Threading;
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
 using Coin.Sdk.Common.Client;
 using Coin.Sdk.NP.Messages.V1;
 using Coin.Sdk.NP.Service.Impl;
@@ -11,6 +13,7 @@ namespace Coin.Sdk.NP.Tests
     public class MessageConsumerTests
     {
         private NumberPortabilityMessageConsumer _messageConsumer;
+        private StopStreamService _stopStreamService;
         private TestListener _listener;
 
         [SetUp]
@@ -18,7 +21,8 @@ namespace Coin.Sdk.NP.Tests
         {
             var logger = NullLogger.Instance;
 
-            var sseConsumer = new SseConsumer(logger, Consumer, SseUrl, PrivateKeyFile, EncryptedHmacSecretFile);
+            var sseConsumer = new SseConsumer(logger, Consumer, SseUrl, PrivateKeyFile, EncryptedHmacSecretFile, numberOfRetries: NumberOfRetries);
+            _stopStreamService = new StopStreamService(ApiUrl, Consumer, PrivateKeyFile, EncryptedHmacSecretFile);
             _messageConsumer = new NumberPortabilityMessageConsumer(sseConsumer, logger);
             _listener = new TestListener();
         }
@@ -33,6 +37,25 @@ namespace Coin.Sdk.NP.Tests
             }
 
             _messageConsumer.StopConsuming();
+        }
+
+        [Test]
+        public void ConsumeUnconfirmedWithInterrupt()
+        {
+            var numberMessages = 0;
+            _listener.SideEffect = messageId =>
+            {
+                numberMessages++;
+                if (messageId == "5") _stopStreamService.StopStream();
+            };
+            _messageConsumer.StartConsumingUnconfirmed(_listener, e => Assert.Fail("Disconnected"));
+            for (var i = 0; i < 30; i++)
+            {
+                Thread.Sleep(1000);
+            }
+
+            _messageConsumer.StopConsuming();
+            Assert.Greater(numberMessages, 20);
         }
 
         [Test]
--- number-portability-sdk-tests/TestUtils.cs
+++ number-portability-sdk-tests/TestUtils.cs
@@ -1,8 +1,13 @@
 using System;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Coin.Sdk.Common;
 using Coin.Sdk.Common.Client;
 using Coin.Sdk.NP.Messages.V1;
 using Coin.Sdk.NP.Service;
+using static Coin.Sdk.Common.Crypto.CtpApiClientUtil;
 
 namespace Coin.Sdk.NP.Tests
 {
@@ -19,6 +24,7 @@ namespace Coin.Sdk.NP.Tests
     {
         public const string PrivateKeyFile = "../../../../keys/private-key.pem";
         public const string EncryptedHmacSecretFile = "../../../../keys/sharedkey.encrypted";
+        public const int NumberOfRetries = 1;
 
         public static string ApiUrl =
             "http://" + (Environment.GetEnvironmentVariable("STUB_HOST_AND_PORT") ?? "localhost:8000") +
@@ -34,71 +40,118 @@ namespace Coin.Sdk.NP.Tests
         public static readonly string Timestamp = DateTime.Now.ToString("yyyyMMddhhmmss");
     }
 
+    public class StopStreamService : CtpApiRestTemplateSupport
+    {
+        private readonly Uri _apiUrl;
+
+        public StopStreamService(string apiUrl, string consumerName, string privateKeyFile,
+            string encryptedHmacSecretFile, string privateKeyFilePassword = null)
+            : this(new Uri(apiUrl), consumerName, ReadPrivateKeyFile(privateKeyFile, privateKeyFilePassword),
+                encryptedHmacSecretFile)
+        {
+        }
+
+        public StopStreamService(Uri apiUrl, string consumerName, RSA privateKey, string encryptedHmacSecretFile,
+            string privateKeyFilePassword = null)
+            : this(apiUrl, consumerName, HmacFromEncryptedBase64EncodedSecretFile(encryptedHmacSecretFile, privateKey),
+                privateKey)
+        {
+        }
+
+        public StopStreamService(Uri apiUrl, string consumerName, HMACSHA256 signer, RSA privateKey) : base(
+            consumerName, privateKey, signer) => _apiUrl = apiUrl;
+        
+        public void StopStream()
+        {
+            SendWithTokenAsync(HttpMethod.Get, _apiUrl.AddPathArg("dossiers/stopstream"), "");
+        }
+    }
+
     public class TestListener : INumberPortabilityMessageListener
     {
+        private Action<string> _sideEffect = _ => { };
+
+        public Action<string> SideEffect
+        {
+            set => _sideEffect = value;
+        }
+
         public void OnActivationServiceNumber(string messageId, ActivationServiceNumberMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnCancel(string messageId, CancelMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnDeactivation(string messageId, DeactivationMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnDeactivationServiceNumber(string messageId, DeactivationServiceNumberMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumActivationNumber(string messageId, EnumActivationNumberMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumActivationOperator(string messageId, EnumActivationOperatorMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumActivationRange(string messageId, EnumActivationRangeMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumDeactivationNumber(string messageId, EnumDeactivationNumberMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumDeactivationOperator(string messageId, EnumDeactivationOperatorMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumDeactivationRange(string messageId, EnumDeactivationRangeMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumProfileActivation(string messageId, EnumProfileActivationMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnEnumProfileDeactivation(string messageId, EnumProfileDeactivationMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnErrorFound(string messageId, ErrorFoundMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnException(Exception exception)
@@ -114,36 +167,43 @@ namespace Coin.Sdk.NP.Tests
         public void OnPortingPerformed(string messageId, PortingPerformedMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnPortingRequest(string messageId, PortingRequestMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnPortingRequestAnswer(string messageId, PortingRequestAnswerMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnPortingRequestAnswerDelayed(string messageId, PortingRequestAnswerDelayedMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnRangeActivation(string messageId, RangeActivationMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnRangeDeactivation(string messageId, RangeDeactivationMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnTariffChangeServiceNumber(string messageId, TariffChangeServiceNumberMessage message)
         {
             Debug.WriteLine($"Received message with id {messageId} of type {message.GetType()}");
+            _sideEffect(messageId);
         }
 
         public void OnUnknownMessage(string messageId, string message)
